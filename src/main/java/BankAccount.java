6b3cd2aa333560eb3fee7d3051603fa34bb8e109
Sahil Patel <sahilpatel1906@gmail.com>
2024-02-07 10:18:47 -0500

    first commit

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,35 @@
+//import java.time.LocalDate;
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private int balance;
+
+    public BankAccount(String firstName, String lastName, String dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = 0;
+        this.balance = 0;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // Getters and Setters
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String changedFirstName) {
+        this.firstName = changedFirstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String changedLastName) {
+        this.lastName = changedLastName;
+    }
+
+}
---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BankAccountTest {
+    private BankAccount account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new BankAccount("Bob", "The Builder", "19/01/1999");
+    }
+
+    @Test
+    public void canGetFirstName() {
+        //Arrange
+        String expected_name = "Bob";
+        //ACT
+        String actual_name = account.getFirstName();
+        //ASSERT
+        assertThat(actual_name).isEqualTo(expected_name);
+    }
+
+    @Test
+    public void canSetFirstName() {
+        String expected_name = "Bobby";
+        account.setFirstName("Bobby");
+        String actual_name = account.getFirstName();
+        assertThat(actual_name).isEqualTo(expected_name);
+    }
+
+    @Test
+    public void canGetLastName() {
+        String expected_name = "The Builder";
+        String actual_name = account.getLastName();
+        assertThat(actual_name).isEqualTo(expected_name);
+    }
+
+    @Test
+    public void canSetLastName() {
+        String expected_name = "Builder";
+        account.setLastName(expected_name);
+        String actual_name = account.getLastName();
+        assertThat(actual_name).isEqualTo(expected_name);
+    }
+
+}
